603a31707bd4f0c790294f10ffc76701001deca8
JeremiePB <jeremiepb@gmail.com>
2024-06-26 14:28:11 -0400

    Add project files.

---
+++ Proto/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace Proto
+{
+    partial class OU_Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            richTextBox1 = new RichTextBox();
+            panel1 = new Panel();
+            button13 = new Button();
+            button12 = new Button();
+            button11 = new Button();
+            richTextBox2 = new RichTextBox();
+            richTextBox3 = new RichTextBox();
+            richTextBox4 = new RichTextBox();
+            richTextBox5 = new RichTextBox();
+            richTextBox6 = new RichTextBox();
+            richTextBox7 = new RichTextBox();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(574, 103);
+            button1.Name = "button1";
+            button1.Size = new Size(214, 39);
+            button1.TabIndex = 0;
+            button1.Text = "Wifi";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(574, 148);
+            button2.Name = "button2";
+            button2.Size = new Size(214, 39);
+            button2.TabIndex = 0;
+            button2.Text = "Écran";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button1_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(574, 193);
+            button3.Name = "button3";
+            button3.Size = new Size(214, 39);
+            button3.TabIndex = 0;
+            button3.Text = "Clavier";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button1_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(574, 238);
+            button4.Name = "button4";
+            button4.Size = new Size(214, 39);
+            button4.TabIndex = 0;
+            button4.Text = "Audio";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button1_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(574, 283);
+            button5.Name = "button5";
+            button5.Size = new Size(214, 39);
+            button5.TabIndex = 0;
+            button5.Text = "Webcam";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button1_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(6, 358);
+            button6.Name = "button6";
+            button6.Size = new Size(214, 39);
+            button6.TabIndex = 0;
+            button6.Text = "Drivers";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button1_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(226, 358);
+            button7.Name = "button7";
+            button7.Size = new Size(214, 39);
+            button7.TabIndex = 0;
+            button7.Text = "BIOS";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button1_Click;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(6, 403);
+            button8.Name = "button8";
+            button8.Size = new Size(214, 39);
+            button8.TabIndex = 0;
+            button8.Text = "Windows Update";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button1_Click;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(226, 403);
+            button9.Name = "button9";
+            button9.Size = new Size(214, 39);
+            button9.TabIndex = 0;
+            button9.Text = "BiosVC";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button1_Click;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(574, 328);
+            button10.Name = "button10";
+            button10.Size = new Size(214, 39);
+            button10.TabIndex = 0;
+            button10.Text = "Mémoire";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button1_Click;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(0, 94);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(534, 32);
+            richTextBox1.TabIndex = 1;
+            richTextBox1.Text = "Modèle";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.Desktop;
+            panel1.Controls.Add(button13);
+            panel1.Controls.Add(button12);
+            panel1.Controls.Add(button11);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 94);
+            panel1.TabIndex = 2;
+            // 
+            // button13
+            // 
+            button13.Location = new Point(620, 10);
+            button13.Name = "button13";
+            button13.Size = new Size(81, 72);
+            button13.TabIndex = 0;
+            button13.Text = "Wifi Status";
+            button13.UseVisualStyleBackColor = true;
+            button13.Click += button12_Click;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(707, 10);
+            button12.Name = "button12";
+            button12.Size = new Size(81, 72);
+            button12.TabIndex = 0;
+            button12.Text = "USB Status";
+            button12.UseVisualStyleBackColor = true;
+            button12.Click += button12_Click;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(8, 10);
+            button11.Name = "button11";
+            button11.Size = new Size(81, 72);
+            button11.TabIndex = 0;
+            button11.Text = "Battery%";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox2
+            // 
+            richTextBox2.Location = new Point(0, 126);
+            richTextBox2.Name = "richTextBox2";
+            richTextBox2.Size = new Size(534, 32);
+            richTextBox2.TabIndex = 1;
+            richTextBox2.Text = "Numéro Série";
+            // 
+            // richTextBox3
+            // 
+            richTextBox3.Location = new Point(0, 158);
+            richTextBox3.Name = "richTextBox3";
+            richTextBox3.Size = new Size(534, 32);
+            richTextBox3.TabIndex = 1;
+            richTextBox3.Text = "Version BIOS";
+            // 
+            // richTextBox4
+            // 
+            richTextBox4.Location = new Point(0, 190);
+            richTextBox4.Name = "richTextBox4";
+            richTextBox4.Size = new Size(534, 32);
+            richTextBox4.TabIndex = 1;
+            richTextBox4.Text = "Processeur (CPU)";
+            // 
+            // richTextBox5
+            // 
+            richTextBox5.Location = new Point(0, 222);
+            richTextBox5.Name = "richTextBox5";
+            richTextBox5.Size = new Size(534, 32);
+            richTextBox5.TabIndex = 1;
+            richTextBox5.Text = "Mémoire (RAM)";
+            richTextBox5.TextChanged += richTextBox5_TextChanged;
+            // 
+            // richTextBox6
+            // 
+            richTextBox6.Location = new Point(0, 254);
+            richTextBox6.Name = "richTextBox6";
+            richTextBox6.Size = new Size(534, 32);
+            richTextBox6.TabIndex = 1;
+            richTextBox6.Text = "Disque Dur";
+            // 
+            // richTextBox7
+            // 
+            richTextBox7.Location = new Point(0, 286);
+            richTextBox7.Name = "richTextBox7";
+            richTextBox7.Size = new Size(534, 32);
+            richTextBox7.TabIndex = 1;
+            richTextBox7.Text = "GPU (Obsolete?)";
+            // 
+            // OU_Main
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDarkDark;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Controls.Add(richTextBox7);
+            Controls.Add(richTextBox5);
+            Controls.Add(richTextBox6);
+            Controls.Add(richTextBox4);
+            Controls.Add(richTextBox3);
+            Controls.Add(richTextBox2);
+            Controls.Add(richTextBox1);
+            Controls.Add(button10);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button1);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            KeyPreview = true;
+            Name = "OU_Main";
+            RightToLeft = RightToLeft.No;
+            Text = "OutilUpdate Ver. 2.0.0";
+            Load += OU_Main_Load;
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private RichTextBox richTextBox1;
+        private Panel panel1;
+        private RichTextBox richTextBox2;
+        private RichTextBox richTextBox3;
+        private RichTextBox richTextBox4;
+        private RichTextBox richTextBox5;
+        private RichTextBox richTextBox6;
+        private RichTextBox richTextBox7;
+        private Button button12;
+        private Button button11;
+        private Button button13;
+    }
+}
---
+++ Proto/Form1.cs
@@ -0,0 +1,35 @@
+namespace Proto
+{
+    public partial class OU_Main : Form
+    {
+        public OU_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OU_Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
